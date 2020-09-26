6b5a6e9563207b812fa39c368e6ce8000260c93f
fonzynice <apple@Pro.local>
2020-09-26 11:28:15 -0400

    templates folder added

--- myapp.py
+++ myapp.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return '<h1> HOME PAGE </h1>'
+    return render_template('home.html')
+
+
+@app.route("/nornir")
+def nornir():
+    return render_template('nornir.html')
 
 
 if __name__ == '__main__':
